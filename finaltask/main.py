8cd6f8a3566f202afcac0b1d4b5fecc0c5d92470
Alpaphono <ashl3yaoa@gmail.com>
2024-05-01 14:43:23 +0100

    finally finished the app.

--- finaltask/main.py
+++ finaltask/main.py
@@ -10,8 +10,14 @@ while True:
     if usr_input.upper() == "Y":
         print(menu)
         prompt = "Please enter a password to check its strength: "
-        usr_password = pw.Password_checker(input(prompt))
-        usr_password.pattern_search()
+        usr_input = input(prompt)
+
+        if usr_input not in pw.Password_checker.very_weak_passwords:
+            usr_password = pw.Password_checker(usr_input)
+            
+            usr_password.pass_rank(usr_password.how_many_special_characters(),usr_password.length_rating(),usr_password.pattern_search())
+        else:
+            print("This is a VERY WEAK PASSWORD")
     elif usr_input.upper() == "N":
         print("Thank you, Goodbye!")
         break
--- finaltask/password_checker.py
+++ finaltask/password_checker.py
@@ -20,40 +20,33 @@ import re
 
 
 class Password_checker():
+
+    very_weak_passwords = ['123456','123456789','qwerty','password','qwerty123','password1','1q2w3e','hello','passw0rd','PassWord']
+
     def __init__(self,password):
-
-        self.password = password
-
-    def pass_length(self):
-        '''
-        Finds the length of the password
-        
-        Args:
-            self: object of the class
-
-        Returns:
-            length (int): The number of characters in the password
-        '''
-
-        password = self.password
-        length = len(password)
-        return length
+        self.password = password   
     
-    def length_rating(cls,password):
+    def length_rating(self):
+        pass_length = len(self.password)
+        length_rating = 0
+        if 0 < pass_length < 5:
+            length_rating = 1
+        elif 4 < pass_length < 8:
+            length_rating = 2
+        elif 7 < pass_length < 10:
+            length_rating = 3
+        elif 9 < pass_length: 
+            length_rating = 5
+        return length_rating
 
-        if 1 < cls(password).pass_length() < 5:
-            print("password is weak") 
-        elif 5 < cls(password).pass_length() < 10:
-            print("password is medium")
-        else:
-            print("password is strong")
-
+        
 
         
 
 
     def pattern_search(self):
         password = self.password
+        pattern_rating = 0
 
         strongest_pattern = r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d+)(?!=.*\s).*"
         strongest_match = re.search(strongest_pattern,password)
@@ -68,14 +61,49 @@ class Password_checker():
         weakest_match = re.search(weakest_pattern,password)
 
         if strongest_match:
-            print("This is a strong password")
+            pattern_rating = 4
+            #print("This is a strong password")
         elif average_match:
-            print("This is an average password")
+            pattern_rating = 3
+            #print("This is an average password")
         elif weak_match:
-            print("This is a weak password")
+            #print("This is a weak password")
+            pattern_rating = 2
         elif weakest_match:
-            print("This password is too weak.")
+            pattern_rating = 1
+            #print("This password is too weak.")
+        
+        return  pattern_rating
 
+    def how_many_special_characters(self):
+        password = self.password
+        spec_char = 0
+        char_rating = 0
+        for char in password:
+            if not char.isalnum() and not char.isspace():
+                spec_char += 1
+        
+        if spec_char == 0:
+            char_rating = 1
+        elif spec_char < 3:
+            char_rating = 2
+        elif spec_char < 4:
+            char_rating = 3
+        elif spec_char > 5:
+            char_rating = 4
 
-    def password_ranking(self):
-        pass
+        return char_rating 
+    
+    def pass_rank(self, len_rate, pattern_rate, char_rate):
+    
+        final_rating = len_rate + pattern_rate + char_rate
+
+        if 0 < final_rating < 5:
+            print("weak")
+        elif 4 < final_rating < 8:
+            print("Medium")
+        elif 7 < final_rating < 12:
+            print("Strong")
+        elif 11 < final_rating < 16:
+            print("Very Strong")
+    
