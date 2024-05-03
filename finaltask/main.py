b2c39ce880a2f209f1f0d91e36e9729a356df9ce
Alpaphono <ashl3yaoa@gmail.com>
2024-05-04 00:15:04 +0100

    added comments

--- finaltask/main.py
+++ finaltask/main.py
@@ -1,23 +1,33 @@
+'''
+This file is the main file from which my password checker is run.
+Technically It could just all go into one file if wer're being honest.
+'''
 import password_checker as pw
 
 
-menu = '''
+MENU = '''
 Welcome to the password checker!
 Either enter your password to check its strength.\n
 '''
-while True: 
+
+#--------------------------------------------------------------------
+# This While loop executes the menu in which the user interacts with
+# Also execures the methods from the password checker class
+#--------------------------------------------------------------------
+while True:
     usr_input = input("Would you like to check your password (Y/N)")
     if usr_input.upper() == "Y":
-        print(menu)
-        prompt = "Please enter a password to check its strength: "
-        usr_input = input(prompt)
+        print(MENU)
+        PROMPT = "Please enter a password to check its strength: "
+        usr_input = input(PROMPT)
 
         if usr_input not in pw.Password_checker.very_weak_passwords:
             usr_password = pw.Password_checker(usr_input)
-            
-            usr_password.pass_rank(usr_password.how_many_special_characters(),usr_password.length_rating(),usr_password.pattern_search())
-        else:
-            print("This is a VERY WEAK PASSWORD")
+            usr_password.pass_rank(
+                usr_password.how_many_special_characters(),
+                usr_password.length_rating(),
+                usr_password.pattern_search())
+        else: print("This is a VERY WEAK PASSWORD")
     elif usr_input.upper() == "N":
         print("Thank you, Goodbye!")
         break
--- finaltask/password_checker.py
+++ finaltask/password_checker.py
@@ -20,13 +20,35 @@ import re
 
 
 class Password_checker():
+    '''
+    This Class represents a password object and methods that determine its strength
+    '''
 
-    very_weak_passwords = ['123456','123456789','qwerty','password','qwerty123','password1','1q2w3e','hello','passw0rd','PassWord']
+    # List of weak passwords that will be used to automatically give,
+    # User a 'weak' rating if entered
+    very_weak_passwords = [
+        '123456','123456789','qwerty','password',
+        'qwerty123','password1','1q2w3e','hello','passw0rd','PassWord'
+        ]
 
     def __init__(self,password):
+        '''
+        Initialises a password object 
+        
+        Args:
+            password(str): The string that the user enters to suggest as a password
+
+        '''
         self.password = password   
     
     def length_rating(self):
+        '''
+        Determines the length of the password string 
+        
+        Return:
+            length_rating(int): numeric number to determine strength of the string 
+                                based on length
+        '''
         pass_length = len(self.password)
         length_rating = 0
         if 0 < pass_length < 5:
@@ -45,10 +67,18 @@ class Password_checker():
 
 
     def pattern_search(self):
+        '''
+        Determines the strength of the password based on what chars are 
+        used to create the string.
+
+        Returns:
+            pattern_rating(int): numeric number to determine the strength of the string
+        '''
         password = self.password
         pattern_rating = 0
 
-        strongest_pattern = r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d+)(?!=.*\s).*"
+        strongest_pattern = r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d+)(?!=.*\s).*" # RegEx matches if the string has caps and lowercase digits and special characters 
+                                                                          # using positive lookahead (?=..) and negative lookahead (?!=..)
         strongest_match = re.search(strongest_pattern,password)
 
         average_pattern =r"(?=.*[a-z])(?=.*\d+)(?!=.*\s)(?!=.*[A-Z]).*"
@@ -76,6 +106,13 @@ class Password_checker():
         return  pattern_rating
 
     def how_many_special_characters(self):
+        '''
+        Counts how many special characters are in the password object
+        and gives a numeric rating based on that.
+
+        Returns:
+            char_rating(int): numeric number used to determine password rating
+        '''
         password = self.password
         spec_char = 0
         char_rating = 0
@@ -95,6 +132,14 @@ class Password_checker():
         return char_rating 
     
     def pass_rank(self, len_rate, pattern_rate, char_rate):
+        '''
+        Takes a total of different rating values and uses the number to determine overall password strength.
+        
+        Args:
+            len_rate(int): number representing the score for the length of the pw. Returned from length_rating method
+            pattern_rate(int): number representing the score for the complexity of the password. Returned from the pattern_search() method
+            char_rate(int): Number representing the score for the number of special characters used in the password. Returned from the how_many_special_characters() method
+        '''
     
         final_rating = len_rate + pattern_rate + char_rate
 
